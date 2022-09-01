cffe85295a412758ae2761dc62a7c9e203889b45
Nicolas Barbudez <nico.barbudez95@gmail.com>
2022-09-01 18:39:38 -0300

    agregue Item y ItemList

--- my-app/src/components/ItemList.js
+++ my-app/src/components/ItemList.js
@@ -1,22 +1,27 @@
-import { React } from 'react';
+import { React, useEffect, useState } from 'react';
 import Item from './Item';
 
 
-function ItemList(props) {
+function ItemList() {
 
-return(
-<>
-    <section className="cuerpo">
-      <div>
-        <h2>Catalogo de productos</h2>
-        <p>Elegi tus productos de esta lista:</p>
-      </div>
-      <div className="catalogo">
-        
-      </div>
-    </section>
-</>
+  const[info, setInfo] = useState([])
+
+  useEffect(() => {
+    setTimeout(() =>
+    fetch('data.json')
+    .then((resp) => resp.json())
+    .then((data) => setInfo(data)),
+  2000
   );
+  }, [])
+
+return (
+  <>
+
+      {info && info.map(i => <Item id={i.nombre} imagen={i.imagen} precio={i.precio} descripcion={i.descripcion} />)}
+
+  </>
+)
 }
 
 export default ItemList;
--- my-app/src/components/ItemListContainer.js
+++ my-app/src/components/ItemListContainer.js
@@ -1,31 +1,16 @@
-import{useEffect, useState} from 'react';
-import Item from './Item';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-
+import ItemList from './ItemList';
 
 
 function ItemListContainer (){
 
-  const[info, setInfo] = useState([])
+  return (
+    <>
+      <ItemList/>
+    </>
+  )
 
-  useEffect(() => {
-    setTimeout(() =>
-    fetch('data.json')
-    .then((resp) => resp.json())
-    .then((data) => setInfo(data)),
-  3000
-  );
-  }, [])
-
-return (
-  <>
-
-      {info && info.map(i => <Item id={i.nombre} imagen={i.imagen} precio={i.precio} descripcion={i.descripcion} />)}
-
-  </>
-)
     }
 
 export default ItemListContainer;
