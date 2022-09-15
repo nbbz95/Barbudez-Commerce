4bd505fd49b9248aa0bac4a16b361fc8639041e9
Nicolas Barbudez <nico.barbudez95@gmail.com>
2022-09-14 21:36:43 -0300

    sincronizacion del counter

--- my-app/src/components/ItemDetail.jsx
+++ my-app/src/components/ItemDetail.jsx
@@ -1,11 +1,31 @@
+/* eslint-disable jsx-a11y/alt-text */
+import { useState } from "react";
+import ItemCount from "./ItemCount";
+
 const ItemDetail = ({ items }) => {
+    const [add, setAdd] = useState(false)
+    const onAdd = () => {
+        setAdd(!add)
+    }
+
     return (
-    <div>
-    <img src={items.thumbnail}/>
-     <h5>{items.title} </h5>
-     <h6> {items.id}</h6>
-     <p>${items.price}</p>
-     </div>
+    <>
+        <div>
+            <img src={items.thumbnail} />
+            <h5>{items.title} </h5>
+            <h6> {items.id}</h6>
+            <p>${items.price}</p>
+        </div>
+
+        <div>
+            {
+                add ?
+                ""
+                :
+                 <ItemCount stock={10} initial={1} onAdd={onAdd} />
+            }
+        </div>
+            </>
     )
     }
      
